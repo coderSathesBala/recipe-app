9436b1e41d2a7bad2955bd2c6946490814e6ea22
Sathes Balasingam <sathes352@gmail.com>
2019-07-10 14:04:27 -0400

    AddAPIKeyBeginningProject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '699ae5b0'
+  const APP_KEY = '89e8ba8a39644a25a21deb6e2146dc2f'
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('effect')
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form className="search-form">
+          <input className="search-bar" type="text"/>
+          <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">
+            {counter}
+          </button>
+        </form>
     </div>
   );
 }

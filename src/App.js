174eedadf30b28733bacb63368199b9d8b9efdc0
Sathes Balasingam <sathes352@gmail.com>
2019-07-11 11:05:03 -0400

    WorkingWithAPI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe'
 
 const App = () => {
 
   const APP_ID = '699ae5b0'
   const APP_KEY = '89e8ba8a39644a25a21deb6e2146dc2f'
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const [counter, setCounter] = useState(0)
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log('effect')
-  })
+    getRecipes();
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch (`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits)
+  }
 
   return (
     <div className="App">
         <form className="search-form">
           <input className="search-bar" type="text"/>
-          <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">
-            {counter}
-          </button>
+          <button className="search-button" type="submit"> Search </button>
         </form>
+        {recipes.map(recipe => (
+          <Recipe />
+        ))}
     </div>
   );
 }

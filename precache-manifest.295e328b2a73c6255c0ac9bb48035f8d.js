20a9bfd41a9be23fe55cbd903a0942bf2b5ceeb3
Sathes Balasingam <sathes352@gmail.com>
2019-07-13 11:21:30 -0400

    Updates

---
+++ precache-manifest.295e328b2a73c6255c0ac9bb48035f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2826073bb6285a7df0922c2b4b6127e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "7cfab41babe30d23b460",
+    "url": "/recipe-app/static/css/main.2af10023.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipe-app/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "7cfab41babe30d23b460",
+    "url": "/recipe-app/static/js/main.7a8ab8c0.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

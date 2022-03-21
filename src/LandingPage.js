19fb0533846814fc90ba2519501cc61f0fad6838
coderSathesBala <sathes352@gmail.com>
2022-03-21 19:40:42 -0400

    AddedLandingPageText

---
+++ src/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+    return (
+        <div className="landingPage">
+            <h1>ALL RECIPES</h1>
+            <h2>A platform to assist you choose your favorite recipe!</h2>
+            <h2>We're building a recipe community where people discover food, share recipes, and connect with creators.</h2>
+            <h1>GET STARTED</h1>
+        </div>
+    )
+}
+
+export default LandingPage;

e993b00c1b0d6cba016d4adc164bae1e943fdd38
coderSathesBala <sathes352@gmail.com>
2022-03-22 16:06:42 -0400

    addBackgroundImage

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import background from "./landingPageImage.jpeg";
 
 const LandingPage = () => {
 
+    const divStyle = {
+            
+      backgroundImage: `url("https://via.placeholder.com/500")`,
+      };
+
     return (
-        <div className="landingPage">
+        <div style={{ 
+            backgroundImage: `url(${background})`,
+            height: `57rem`,
+            color: `yellow`
+            }}>
             <h1>ALL RECIPES</h1>
             <h2>A platform to assist you choose your favorite recipe!</h2>
             <h2>We're building a recipe community where people discover food, share recipes, and connect with creators.</h2>
